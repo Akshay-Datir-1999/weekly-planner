af8e7bf47e32b64ba7c98716ef7f3d42a4b1f579
Akshay Datir <datirad1999@gmail.com>
2026-03-03 09:57:55 +0530

    Update

--- weekly-planner-ui/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { PlanningComponent } from './features/planning/planning.component';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: PlanningComponent
-  }
-];
--- weekly-planner-ui/src/app/core/guards/lead-guard.ts
+++
@@ -1,5 +0,0 @@
-import { CanActivateFn } from '@angular/router';
-
-export const leadGuard: CanActivateFn = (route, state) => {
-  return true;
-};
