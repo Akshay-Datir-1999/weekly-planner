1f000415f1d5c868b77e071b0205b475a526a2b5
Akshay Datir <datirad1999@gmail.com>
2026-03-05 18:28:18 +0530

    feat: completed frontend services, routes and configuration updates

--- weekly-planner-ui/src/app/app.config.ts
+++ weekly-planner-ui/src/app/app.config.ts
@@ -1,17 +1,11 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RoleInterceptor } from './interceptors/role.interceptor';
-import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi()),
-    provideAnimations(),
-    { provide: HTTP_INTERCEPTORS, useClass: RoleInterceptor, multi: true }
-  ]
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter([]),
+    provideHttpClient(),
+  ],
 };
